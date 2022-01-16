1c860acf957323388e8a52de4085c553529a9788
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-01-16 13:13:24 +0000

    Edited pepcon_handout.html with Atlas code editor

--- argparse/test.sh
+++ argparse/test.sh
@@ -24,4 +24,12 @@ argv[0]: do
 argv[1]: -f
 argv[2]: -h'
 
-is "$(./test_argparse -tf 2>
+is "$(./test_argparse -tf 2>&1)" 'force: 1
+test: 1'
+
+is "$(./test_argparse --read --write 2>&1)" 'perms: 3'
+
+is "$(./test_argparse -h)" 'Usage: test_argparse [options] [[--] args]
+   or: test_argparse [options]
+
+A brief description of what the 
