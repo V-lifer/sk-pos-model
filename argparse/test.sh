9a9413f499c206e5741c2c44e5118b5e61376dfc
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-01-16 05:43:11 +0000

    hpfs: better test for errors

--- argparse/test.sh
+++ argparse/test.sh
@@ -8,4 +8,11 @@ path: /path/to/file
 argc: 1
 argv[0]: a'
 
-is "$(./test_argpar
+is "$(./test_argparse -f -f --force --no-force 2>&1)" 'force: 2'
+
+is "$(./test_argparse -n 2>&1)" 'error: option `-n` requires a value'
+
+is "$(./test_argparse -n 2 2>&1)" 'num: 2'
+
+is "$(./test_argparse -n2 2>&1)" 'num: 2'
+
