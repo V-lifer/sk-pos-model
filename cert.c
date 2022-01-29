64747f82dcb390852df6c74e2fc2dac17d44d81a
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-01-29 20:36:44 +0000

    Nightly

--- cert.c
+++ cert.c
@@ -2,4 +2,11 @@
 
 struct SignedCert *
 cert_build_cert(const uint8_t *crypt_publickey, int cert_file_expire_seconds,
-                int us
+                int use_xchacha20)
+{
+    struct SignedCert *signed_cert = malloc(sizeof(struct SignedCert));
+    if (!signed_cert)
+        return NULL;
+
+    memcpy(signed_cert->magic_cert, CERT_MAGIC_CERT, 4);
+    signed_cert->ver
