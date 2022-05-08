78412c26de572b28c66d055a07f279441f5ca017
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-05-08 17:48:46 +0000

    remove obsolete test

--- debug.h
+++ debug.h
@@ -1,3 +1,12 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 /**
+ * Debug facilities.
+ */
+
+#include "compat.h"
+
+#define debug_assert(e) ((e) ? (void)0 : (_debug_assert(#e, __FILE__, __LINE__), _exit(1)))
+void _debug_assert(char *err, char *file, int line);
+
+void debug_init(v
