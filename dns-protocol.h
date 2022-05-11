005ea98f9d20ea423479ec23a79aaca31756e27d
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-05-11 15:16:30 +0000

    Finally working

--- dns-protocol.h
+++ dns-protocol.h
@@ -2,4 +2,7 @@
 #define DNS_PROTOCOL_H
 /* dnsmasq is Copyright (c) 2000-2012 Simon Kelley
 
-   This program is free software; yo
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 dated June, 1991, or
+   (at your option) version 3 dated 29 June, 20
