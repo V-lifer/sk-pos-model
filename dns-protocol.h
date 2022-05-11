52f9d1a6f76425268642690c39b84c0e41c2d76e
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-05-11 21:43:31 +0000

    Dockerfile added

--- dns-protocol.h
+++ dns-protocol.h
@@ -5,4 +5,8 @@
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; version 2 dated June, 1991, or
-   (at your option) version 3 dated 29 June, 20
+   (at your option) version 3 dated 29 June, 2007.
+ 
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PU
