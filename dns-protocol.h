0d8a48fbf822194373a898fa48ea99a1a58b2ebc
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-05-13 17:04:38 +0000

    update Comlib

--- dns-protocol.h
+++ dns-protocol.h
@@ -13,4 +13,15 @@
    GNU General Public License for more details.
      
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <htt
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * DNS Protocol Header
+ * 
+ * file from dnsmasq/src/dns-protocol.h with slight modification.
+ */
+
+#define NAMESERVER_PORT 53
+#define TFTP_PORT       69
+
+#define IN6ADDRSZ   
