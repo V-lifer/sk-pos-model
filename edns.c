7ac11b814b87bad27e49933be13c851dbabac3fd
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-06-03 13:22:35 +0000

    Fixed redirect

--- edns.c
+++ edns.c
@@ -2,4 +2,13 @@
 
 static int
 _skip_name(const uint8_t *const dns_packet, const size_t dns_packet_len,
-           size_t * const offset_
+           size_t * const offset_p)
+{
+    size_t offset = *offset_p;
+    uint8_t name_component_len;
+
+    if (dns_packet_len < (size_t) 1U || offset >= dns_packet_len - (size_t) 1U) {
+        return -1;
+    }
+    do {
+     
