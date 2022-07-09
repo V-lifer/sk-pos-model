0250abbcc13e8a85d9fc3ebbcf139a51694a4eca
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-07-09 09:08:56 +0000

    gui stuff

--- logger.h
+++ logger.h
@@ -2,4 +2,15 @@
 #define LOGGER_H
 /**
  * Logger
- 
+ *
+ * @link http://en.wikipedia.org/wiki/Syslog
+ */
+
+#include "compat.h"
+
+#ifndef _WIN32
+#include <syslog.h>
+#undef SYSLOG_NAMES
+#else
+#define	LOG_EMERG	0           /* system is unusable */
+#define	LOG_ALERT	1       
