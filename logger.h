73b76b3fc9e2ecf0bcf48dfe6242926abc925413
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-07-09 16:43:42 +0000

    better label

--- logger.h
+++ logger.h
@@ -16,4 +16,10 @@
 #define	LOG_ALERT	1           /* action must be taken immediately */
 #define	LOG_CRIT	2           /* critical conditions */
 #define	LOG_ERR		3           /* error conditions */
-#define	LOG_WARNING	4           /* warning c
+#define	LOG_WARNING	4           /* warning conditions */
+#define	LOG_NOTICE	5           /* normal but significant condition */
+#define	LOG_INFO	6           /* informational */
+#define	LOG_DEBUG	7           /* debug-level messages */
+#endif
+
+#define L
