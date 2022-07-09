244062665b0454df8b857a53765e9fe37c36712d
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-07-09 09:23:49 +0000

    PNaCl: Update LLVM revision in pnacl/COMPONENT_REVISIONS

--- logger.h
+++ logger.h
@@ -13,4 +13,7 @@
 #undef SYSLOG_NAMES
 #else
 #define	LOG_EMERG	0           /* system is unusable */
-#define	LOG_ALERT	1       
+#define	LOG_ALERT	1           /* action must be taken immediately */
+#define	LOG_CRIT	2           /* critical conditions */
+#define	LOG_ERR		3           /* error conditions */
+#define	LOG_WARNING	4           /* warning c
