1c8490d8fc66eccfbc5913cf47d1613df87df667
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-07-15 17:18:35 +0000

    CHROMIUM: cpufreq: interactive: validate above_hispeed_delay

--- safe_rw.c
+++ safe_rw.c
@@ -1,2 +1,16 @@
 
-#include <sys/types.
+#include <sys/types.h>
+
+#include <assert.h>
+#include <errno.h>
+#ifndef _WIN32
+# include <poll.h>
+#endif
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "safe_rw.h"
+
+#ifndef _WIN32
+ssize_t
+safe_write(const int fd, const void *const buf
