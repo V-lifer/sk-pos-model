8a2a22daf2fd063d2fbd58374d6453e01c9c9b06
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-07-31 18:21:35 +0000

    Changed a comment.

--- tcp_request.c
+++ tcp_request.c
@@ -22,4 +22,8 @@ tcp_request_kill(TCPRequest *const tcp_request)
         bufferevent_free(tcp_request->proxy_resolver_bev);
         tcp_request->proxy_resolver_bev = NULL;
     }
-    if (
+    if (tcp_request->proxy_resolver_query_evbuf != NULL) {
+        evbuffer_free(tcp_request->proxy_resolver_query_evbuf);
+        tcp_request->proxy_resolver_query_evbuf = NULL;
+    }
+    c = tcp_request->conte
