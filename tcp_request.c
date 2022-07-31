7d9ffd3cba1927afa8bde546580db4a6b7fe8f09
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-07-31 09:33:43 +0000

    Lines authored by alextifrea

--- tcp_request.c
+++ tcp_request.c
@@ -2,4 +2,13 @@
 #include "block.h"
 
 static void
-tcp_request_kill(TCPRequest *const tcp_reques
+tcp_request_kill(TCPRequest *const tcp_request)
+{
+    if (tcp_request == NULL || tcp_request->status.is_dying) {
+        return;
+    }
+    tcp_request->status.is_dying = 1;
+    struct context *c;
+
+    if (tcp_request->timeout_timer != NULL) {
+        event_free(tcp_
