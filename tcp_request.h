5e89fb2a2d2ed36b43d485b9bfbef4de0983a24a
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-10-15 18:18:41 +0000

    Translated using Weblate (Slovenian)

--- tcp_request.h
+++ tcp_request.h
@@ -1,4 +1,16 @@
 #ifndef TCP_REQUEST_H
 #define TCP_REQUEST_H
 
-#inc
+#include "dnscrypt.h"
+
+#define DNS_MAX_PACKET_SIZE_TCP (65535U + 2U)
+
+#ifndef TCP_REQUEST_BACKLOG
+# define TCP_REQUEST_BACKLOG 128
+#endif
+
+struct context;
+struct cert_;
+
+typedef struct TCPRequestStatus_ {
+    bool has_dns_qu
