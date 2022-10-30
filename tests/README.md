0de2a9778c904ee34faf660099b2fedee541c200
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-10-30 20:24:26 +0000

    added recent changes

--- tests/README.md
+++ tests/README.md
@@ -2,4 +2,5 @@
 
 ## References
 
-- aruba: https:/
+- aruba: https://app.cucumber.pro/projects/aruba/
+- cucumber: https://cucumber.io/docs/reference
---
+++ tests/dnscrypt-fuzzer.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env python
+"""
+
+Requirements:
+* https://github.com/openalias/dnscrypt-python
+* https://github.com/warner/python-pure25519/blob/master/misc/djbec.py
+* a query file of the form `qname\tqtype`. Example query file https://nominum.com/measurement-tools/
