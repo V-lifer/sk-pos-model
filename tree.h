1f25c956d87f3c00df7ac82a5f944c04f78b9217
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-11-26 17:37:32 +0000

    add migration file

--- tree.h
+++ tree.h
@@ -3,4 +3,8 @@
  * Copyright 2002 Niels Provos <provos@citi.umich.edu>
  * All rights reserved.
  *
- * Redistribution and use in source and bi
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice
