e9f8f1fa9baf0e7aa496a638d1e44411958c2fd6
V-lifer <95712255+V-lifer@users.noreply.github.com>
2022-12-03 16:13:45 +0000

    8.1.1

--- tree.h
+++ tree.h
@@ -44,4 +44,10 @@ struct name {								\
 #define RB_RED		1
 #define RB_ENTRY(type)							\
 struct {								\
-	struct type *rbe_left;		/* l
+	struct type *rbe_left;		/* left element */		\
+	struct type *rbe_right;		/* right element */		\
+	struct type *rbe_parent;	/* parent element */		\
+	int rbe_color;			/* node color */		\
+}
+
+#define RB_LEFT(elm, field)		(elm)->field.rb
